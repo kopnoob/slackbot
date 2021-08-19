d5026651e65c625d78de1d8469b9a5c55946efae
kopnoob <oysteinkopstad@gmail.com>
2021-08-20 00:09:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import split
 from flask import Flask, request, Response
 
 app = Flask(__name__)
@@ -9,9 +10,15 @@ def homepage():
     andel = 0.6
     aga = 1/1.141
     fp = 1/1.12
-    timer = 150
+    #timer = 150
+
     inputs = request.form.get('text')
-    lonn = float(inputs)*timer*andel*aga*fp
+    splitInput = inputs.split(",")
+
+    timepris = splitInput[0]
+    timer = splitInput[1]
+    
+    lonn = float(timepris)*float(timer)*andel*aga*fp
     
     return str(lonn)
 
