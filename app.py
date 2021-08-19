429888779bf09827a7ddb83a4b082f37e67521e1
kopnoob <oysteinkopstad@gmail.com>
2021-08-20 00:01:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ PORT = 4390
 
 @app.route('/', methods=['POST'])
 def homepage():
-    #andel = 0.6
-    #aga = 1/1.141
-    #fp = 1/1.12
-    #timer = 150
+    andel = 0.6
+    aga = 1/1.141
+    fp = 1/1.12
+    timer = 150
     inputs = request.form.get('text')
     lonn = float(inputs)*timer*andel*aga*fp
     return str(lonn)
