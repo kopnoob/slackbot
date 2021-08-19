5a24d75411854052b51e3ebd90079927880dd10f
kopnoob <oysteinkopstad@gmail.com>
2021-08-19 20:49:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ PORT = 4390
 
 @app.route('/', methods=['POST'])
 def homepage(timepris):
-    andel = 0.6
-    aga = 1/1.141
-    fp = 1/1.12
-    timer = 150
-    lonn=timepris*timer*andel*aga*fp
-    return lonn
+    #andel = 0.6
+    #aga = 1/1.141
+    #fp = 1/1.12
+    #timer = 150
+    #lonn=timepris*timer*andel*aga*fp
+    return timepris
 
 
 if __name__ == '__main__':
