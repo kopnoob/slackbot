f9ac0259caf3db83204bbde336452c236eaec3e0
kopnoob <oysteinkopstad@gmail.com>
2021-08-19 20:45:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 PORT = 4390
 
 @app.route('/', methods=['POST'])
-def homepage():
-    return "Howdy hacker!"
+def homepage(timepris):
+    andel = 0.6
+    aga = 1/1.141
+    fp = 1/1.12
+    timer = 150
+    lonn=timepris*timer*andel*aga*fp
+    return lonn
 
 
 if __name__ == '__main__':
