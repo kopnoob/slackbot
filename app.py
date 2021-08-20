85211ebfae229b90a45ad683f681c70daec4be49
kopnoob <oysteinkopstad@gmail.com>
2021-08-20 10:54:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,17 @@ def homepage():
     inputs = request.form.get('text')
     splitInput = re.findall(r'[^,\s]+',inputs)
 
-    timepris = splitInput[0]
-    timer = splitInput[1]
-    
-    lonn = int(float(timepris)*float(timer)*andel*aga*fp)
-    feedback = "{} timer med {} i timepris gir {} i lønn. Bonus og utbytte er også opparbeidet, og kommer i tillegg.".format(timer, timepris, lonn)
-    
-    return str(feedback)
+    if splitInput[1] in locals():
+        timepris = splitInput[0]
+        timer = splitInput[1]
+
+        lonn = int(float(timepris)*float(timer)*andel*aga*fp)
+        feedback = "{} timer med {} i timepris gir {} i lønn. Bonus og utbytte er også opparbeidet, og kommer i tillegg.".format(timer, timepris, lonn)
+
+        return str(feedback)
+    else:
+        feedback = "Legg inn [timepris] og [timer] etter /lønn. Komma og mellomrom er godkjente separatorer."
+        return str(feedback)
 
 
 if __name__ == '__main__':
